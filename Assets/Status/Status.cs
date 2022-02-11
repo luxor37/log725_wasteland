bb6d26bc2271cb91ee8dc091f676cff56238ed7d
Jian Jia <jian.jia.1@ens.etsmtl.ca>
2022-02-11 09:49:19 -0500

    status and status controller

---
+++ Assets/Status/Status.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Status
+{
+    // A status can be a buff, debuff, element application, damage (over time or one time)
+    public class Status : ScriptableObject
+    {
+        float duration;
+        float timer = 0f;
+        float initialDmg;
+        float perSecDmg;
+
+        string name;
+        string animationState;
+        string soundToPlay;
+        string particleToSpawn;
+
+        public StatusController _controller;
+
+        public void StatusTick(float deltaTime)
+        {
+            timer += deltaTime;
+            if (timer > duration)
+            {
+                EndStatus();
+            }
+            
+        }
+
+        void EndStatus()
+        {
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Status/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Status
+{
+    // subclass sandbox (I guess component sandbox in this case) pattern. Statuses should only affect the game by using methods available here.
+    // every component that might be affected by Status should be referenced here.
+    public class StatusController : MonoBehaviour
+    {
+        Status[] statuses;
+
+       // MovementController
+       // AnimationController
+       // ParticleController
+       // SoundController
+       // AIController
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach (var status in statuses)
+            {
+                status.StatusTick(Time.deltaTime);
+            }
+        }
+
+        public Status[] GetStatuses()
+        {
+            return statuses;
+        }
+    }
+}
