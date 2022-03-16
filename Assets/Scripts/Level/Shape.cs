51b6a1de21a38738c9d334e23f9cf30b037e1584
Jian Jia <jian.jia.1@ens.etsmtl.ca>
2022-03-16 16:22:39 -0400

    shape

---
+++ Assets/Scripts/Level/Shape.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace Level
+{
+    [CreateAssetMenu(fileName = "Shape", menuName = "ScriptableObjects/LevelGenerator/Shape", order = 1)]
+    public class Shape : ScriptableObject
+    {
+        public enum AttributeEnum
+        {
+            MAX_SIZE,
+            MIN_SIZE,
+        }
+
+        public enum SymbolEnum
+        {
+            FLOOR,
+            CEILING,
+            LADDER,
+            ENEMY,
+            ITEM,
+            COIN
+        }
+
+        public SymbolEnum Symbol;
+
+        public GameObject ShapeObject; // not just geometric data. instead of abstract shape put gameobject (shortcut)
+
+        [SerializeField]
+        public Dictionary<AttributeEnum, int> Attributes;
+
+        private void Awake()
+        {
+            Attributes = new Dictionary<AttributeEnum, int>();
+        }
+    }
+}
